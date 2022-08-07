278991a8a2610bd4187c72340dc22eaa6327ef85
muhammedsaidckr <muhammedsaidckr@gmail.com>
2022-08-07 21:27:13 +0000

    Add BinanceSettings to project

---
+++ GetTopCoins/Helpers/BinanceSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTopCoins.Helpers
+{
+    public static class BinanceSettings
+    {
+        public static string apiKey = "YOUR-API-KEY";
+        public static string secretKey = "YOUR-SECRET-KEY";
+    }
+}

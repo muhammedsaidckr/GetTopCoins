6466abde1edace5f10d6ad3749b9483e04fe832e
muhammedsaidckr <muhammedsaidckr@gmail.com>
2022-08-09 22:23:20 +0000

    Print the results top coins to the console

--- GetTopCoins/Program.cs
+++ GetTopCoins/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using GetTopCoins.Helpers;
+
+var symbols = new List<string>() {
+    "1INCHBTC", "1INCHUSDT", "AAVEBTC"
+};
+
+var symbolList = await ListOperations.GetSymbolChangeList(symbols);
+
+foreach (var symbol in symbolList)
+{
+    Console.WriteLine($"{symbol.symbol} - {symbol.priceSevenDaysAgo} - {symbol.currentPrice} - {Math.Round((symbol.change * 100), 2)}%");
+}
+
+Console.ReadLine();

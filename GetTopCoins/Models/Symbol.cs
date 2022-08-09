3b11e9cb33bc93a95966bf484da986ee5103c68d
muhammedsaidckr <muhammedsaidckr@gmail.com>
2022-08-09 22:22:58 +0000

    Add Symbol model

---
+++ GetTopCoins/Models/Symbol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTopCoins.Models
+{
+    public class Symbol
+    {
+        public string symbol { get; set; }
+
+        public decimal priceSevenDaysAgo { get; set; }
+        public decimal currentPrice { get; set; }
+        public decimal change { get; set;  }
+    }
+}

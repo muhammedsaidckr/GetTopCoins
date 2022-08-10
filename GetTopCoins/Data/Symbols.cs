d0f5d8193eb91786e3a30db5de9b5db0a9309fbc
muhammedsaidckr <muhammedsaidckr@gmail.com>
2022-08-10 20:55:29 +0000

    Symbols list created

---
+++ GetTopCoins/Data/Symbols.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTopCoins.Data
+{
+    public static class Symbols
+    {
+        public static List<string> symbols = new () {
+            "1INCHBTC", "1INCHUSDT", "AAVEBTC"
+        };
+}
+}

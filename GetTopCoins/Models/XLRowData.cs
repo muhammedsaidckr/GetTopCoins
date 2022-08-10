ba965377cf61331cecf12831fecc45520d4c7680
muhammedsaidckr <muhammedsaidckr@gmail.com>
2022-08-10 22:27:49 +0000

    XL row model initialized

---
+++ GetTopCoins/Models/XLRowData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTopCoins.Models
+{
+    public class XLRowData
+    {
+        public string symbol { get; set; }
+        public decimal initialPrice { get; set; }
+        public decimal finalPrice { get; set; }
+        public string changeRate { get; set; }
+    }
+}
